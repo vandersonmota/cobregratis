dd03a481ccf5461d031da8765b564c85443c5b0d
Vanderson Mota <vanderson.mota@gmail.com>
2011-04-02 09:49:59 -0300

    checkpoint

--- lib/cobregratis/bank_billet.rb
+++ lib/cobregratis/bank_billet.rb
@@ -1,4 +1,8 @@
 module Cobregratis
   class BankBillet < Base
+    def self.create(name, amount, due_date)
+      self.new(:name => name, :amount => amount, :due_date => due_date)
+      self.save
+    end
   end
 end
